771ebb46d33a4ca05c418fba4e58c08f44892999
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-09-08 10:27:13 -0400

    se creo modelos de rol, usuario y  usuarioRol

---
+++ Backend/src/models/usuario.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize"
+import sequelize from "../../config/baseDatos.js"
+
+export const Usuario = sequelize.define('Usuario',{
+  id:{
+    type: DataTypes.INTEGER,
+    autoIncrementIdentity: true,
+    primaryKey: true
+  },
+  nombre:{
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  email:{
+    type: DataTypes.STRING(255),
+    unique: true,
+    allowNull: false
+  },
+    password:{
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  activo:{
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true
+  }
+},{
+  timestamps:true,
+  tableName:"Usuario"
+})
