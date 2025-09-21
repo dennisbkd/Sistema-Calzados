4a0c9533975cd8e2ab1ec7b310e18c380a7b2433
Zgustavoz <gustavoibarra323@gmail.com>
2025-09-21 13:43:29 -0400

    primer push

--- Backend/config/baseDatos.js
+++ Backend/config/baseDatos.js
@@ -1,16 +1,30 @@
 import { Sequelize } from "sequelize";
+import * as dotenv from 'dotenv';
 
-const sequelize = new Sequelize('calzados','root','root',{
-  host:'localhost',
-  dialect: 'mysql'
-})
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,      
+  process.env.DB_USER,      
+  process.env.DB_PASSWORD,  
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    logging: false
+  }
+);
 
 export async function db() {
   try {
-    await sequelize.authenticate()
+    await sequelize.authenticate();
+    console.log("Conexión exitosa a la base de datos PostgreSQL");
   } catch (error) {
-    throw new Error("error al conectarse a la base de datos", error);
+    console.error("Error al conectarse a la base de datos:", error);
+    throw error;
   }
 }
 
-export default sequelize
+export default sequelize;
+
+
