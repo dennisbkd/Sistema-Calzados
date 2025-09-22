85bfa5bca1e06b262d3b1195af2f1067c21d443d
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-09-22 13:00:05 -0400

    se agrego las funcionalidades de solicitar un recuperamiento atravez de la tecnologia nodemailer

--- Backend/src/controller/Auth/autorizacion.js
+++ Backend/src/controller/Auth/autorizacion.js
@@ -4,7 +4,6 @@ export class AutorizacionControlador {
   }
 
   iniciarSesion = async (req, res) => {
-    console.log(req.body)
     const body = req.body
     try {
       const respuesta = await this.autorizacionServicio.iniciarSesion({ body })
@@ -12,6 +11,29 @@ export class AutorizacionControlador {
       return res.status(200).json(respuesta)
     } catch (e) {
       return res.status(500).json({ error: `error en el servidor ${e.message}` })
+    }
+  }
+
+  solicitaRecuperamientoPassword = async (req, res) => {
+    const { email } = req.body
+    try {
+      const respuesta = await this.autorizacionServicio.solicitaRecuperamientoPassword({ email })
+      if (respuesta.mensaje) return res.status(401).json(respuesta.mensaje)
+
+      return res.status(200).json(respuesta)
+    } catch (e) {
+      return res.status(500).json({ mensaje: `Error con el servidor ${e.message}` })
+    }
+  }
+
+  resetearPassword = async (req, res) => {
+    const { token, nuevaPassword } = req.body
+    try {
+      const respuesta = await this.autorizacionServicio.resetearPassword({ token, nuevaPassword })
+      if (respuesta.error) return res.status(401).json(respuesta.error)
+      return res.status(200).json(respuesta)
+    } catch (e) {
+      return res.status(500).json({ mensaje: `Error con el servidor ${e.message}` })
     }
   }
 }
