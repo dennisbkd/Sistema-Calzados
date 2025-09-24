0cf07a9d97a06911542c63f7e1b48430cbc8d765
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-09-24 09:46:04 -0400

    agregando las rutas y toaster

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { AppRouter } from "./AppRouter"
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+function App() {
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+    </QueryClientProvider>
+  )
+}
+
+export default App
