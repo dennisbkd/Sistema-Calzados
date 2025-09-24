d922ee8f0d83838ad6296c0e025590be703d876a
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-09-23 21:40:36 -0400

    configurando las cors

--- Backend/src/main.js
+++ Backend/src/main.js
@@ -2,12 +2,17 @@ import express from 'express'
 import { db } from '../config/baseDatos.js'
 import { rutaUsuario } from './router/usuario.js'
 import { rutaAutorizacion } from './router/autorizacion.js'
+import cors from 'cors'
 
 export const App = ({ usuarioServicio, autorizacionServicio }) => {
   const app = express()
   const port = 3000
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
+  app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  }))
 
   db()
 
