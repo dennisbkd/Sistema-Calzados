baa99a9de7f31f1e45a3272c330b6bb5ab1d0a85
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-09-24 16:18:36 -0400

    realizando la funcion para mandar los datos del usuario al localStorage

--- Frontend/src/auth/hooks/Authmutation.jsx
+++ Frontend/src/auth/hooks/Authmutation.jsx
@@ -1,19 +1,26 @@
 import { useMutation } from "@tanstack/react-query";
 import { IniciarSesion } from "../../api/auth/autorizacion";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router";
+
 
 export const Authmutation = () => {
+  const navigate = useNavigate()
   const mutation = useMutation({
     mutationFn: (data) => IniciarSesion(data),
     onSuccess: (data) => {
       toast.success(data.mensaje)
       localStorage.setItem("token", data.token)
+      localStorage.setItem("usuario", JSON.stringify(data.usuario))
+      navigate('/home', { replace: true })
     },
-    onError: (error, variable, context) => {
-      toast.error(`Error en la llamada de la api ${error.message}`)
+    onError: (error) => {
+      const errorBack = error.response.data.error
+      toast.error(`Error ${errorBack} intente de nuevo`)
     }
   })
 
   return mutation
 }
+
 
