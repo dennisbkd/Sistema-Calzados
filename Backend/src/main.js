219d5b5e782fe7e8cecc312d88bdfa79556a4cab
Zgustavoz <gustavoibarra323@gmail.com>
2025-09-26 15:00:11 -0400

    Gestion Roles

--- Backend/src/main.js
+++ Backend/src/main.js
@@ -2,9 +2,10 @@ import express from 'express'
 import { db } from '../config/baseDatos.js'
 import { rutaUsuario } from './router/usuario.js'
 import { rutaAutorizacion } from './router/autorizacion.js'
+import { rutaRol } from './router/rol.js'
 import cors from 'cors'
 
-export const App = ({ usuarioServicio, autorizacionServicio }) => {
+export const App = ({ usuarioServicio, autorizacionServicio, rolServicio, bitacoraServicio }) => {
   const app = express()
   const port = 3000
   app.use(express.json())
@@ -18,6 +19,7 @@ export const App = ({ usuarioServicio, autorizacionServicio }) => {
 
   app.use('/usuario', rutaUsuario({ usuarioServicio }))
   app.use('/autorizacion', rutaAutorizacion({ autorizacionServicio }))
+  app.use('/rol', rutaRol({ rolServicio, bitacoraServicio }))
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
