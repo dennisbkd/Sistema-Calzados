66f70980e59a1f1abc871a2fd74c5c3be44f6f7b
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-09-29 22:02:07 -0400

    agregando las rutas de solicitar y restablecer aplicando transicion de paginas , tambien se agrego useLocation para poder realizar las transiciones junto AnimatePresence

--- Frontend/src/AppRouter.jsx
+++ Frontend/src/AppRouter.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router"
+import { Route, Routes, useLocation } from "react-router"
 import AutorizacionLayout from "./auth/layout/AutorizacionLayout"
 import { RutaProtegida } from "./auth/utils/RutaProtegida"
 import { RutaPublica } from "./auth/utils/RutaPublica"
 import { DashboardPage } from "./adminDashboard/DashboardPage"
 import { RolesPage } from "./adminDashboard/rol/page/RolPage"
+import { SolicitudPassword } from "./auth/page/SolicitudPassword"
+import { RestablecerPassword } from "./auth/page/RestablecerPassword"
+import { AnimatePresence } from "motion/react"
+import { TrancisionLayout } from "./auth/utils/TrancisionLayout"
+import { NotFound } from "./utils/NotFound"
+import { TrancisionPages } from "./auth/utils/TrancisionPages"
 
 
 export const AppRouter = () => {
+  const location = useLocation()
   return (
-    <BrowserRouter>
-      <Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
         {/* RUTAS PARA PAGINAS QUE NO NECESITAS ESTAR LOGUEADO */}
         <Route element={<RutaPublica redireccionar={"/home"} />}>
           <Route path="/" element={<AutorizacionLayout />} />
-
         </Route>
         {/* RUTAS PARA PAGINAS PROTEGIDAS, USUARIOS QUE TENGAN TOKEN y ROLES */}
         <Route element={<RutaProtegida permitidos={['administrador']} />}>
@@ -23,13 +29,22 @@ export const AppRouter = () => {
           </Route>
         </Route>
 
+        <Route element={<TrancisionLayout />}>
+          <Route path="/solicitar-recuperamiento" element={<SolicitudPassword />} />
+          <Route path="/restablecer-password" element={<RestablecerPassword />} />
+        </Route>
+
+
         <Route element={<RutaProtegida permitidos={['vendedor']} />}>
-          <Route path="/ventas" element={<div>ruta para usuarios con rol de vendedor</div>} />
+          <Route path="/ventas" element={<p>ventas</p>} />
         </Route>
 
         <Route path="/clientes" element={<div>Clientes</div>} />
-      </Routes>
-    </BrowserRouter>
 
+        <Route path="*" element={<TrancisionPages>
+          <NotFound />
+        </TrancisionPages>} />
+      </Routes>
+    </AnimatePresence>
   )
 }
