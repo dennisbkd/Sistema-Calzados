1790c3e1a5b4814cb31215f25f4c13973a22eff0
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-09-29 22:04:23 -0400

    aplicando animacion a el dashboard

--- Frontend/src/adminDashboard/DashboardPage.jsx
+++ Frontend/src/adminDashboard/DashboardPage.jsx
@@ -1,12 +1,26 @@
+/* eslint-disable no-unused-vars */
+import { AnimatePresence, motion } from "motion/react"
 import { SideBar } from "./SideBar"
-import { Outlet } from "react-router"
+import { Outlet, useLocation } from "react-router"
 
 export const DashboardPage = () => {
+  const location = useLocation()
+
   return (
     <div className="flex h-screen">
       <SideBar />
       <main className="flex-1 p-6 bg-gray-50 overflow-auto">
-        <Outlet />
+        <AnimatePresence>
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </main>
     </div>
   )
